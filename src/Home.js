c7a74c24f1a795733c12fffb887104c359d4b2af
tanelam <tania.e.aparicio@gmail.com>
2018-05-02 14:54:38 -0400

    added new screenshot

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ class Home extends React.Component{
             <p>Snakes and Ladders is an interactive version of the worldwide classic. It is a two player board game to play with your friends and family.</p>
             <p>Click on "Game" to see the board and start a new game, or click on "Instructions" to see how to play.</p>
             <p>After you finish the game if you want to play again just click on the button that says "New Game", and you can start over again.</p>
+            <p>Good luck and enjoy the game!</p>
           </div>
 
       )
