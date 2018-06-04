930818d8d5735940ab559005a29843ee810f01c8
tanelam <tania.e.aparicio@gmail.com>
2018-06-04 19:34:30 -0400

    refactored css on buttons

--- src/Dice.js
+++ src/Dice.js
@@ -4,7 +4,7 @@ import "./css/buttons.css"
 const Dice = ( props ) =>{
   return(
     <div>
-      <button  className="button-roll-dice" type="button" onClick={props.diceValue} >Roll the Dice</button>
+      <button  className="button roll-dice" type="button" onClick={props.diceValue} >Roll the Dice</button>
     </div>
   )
 }
--- src/NewGame.js
+++ src/NewGame.js
@@ -4,7 +4,7 @@ import "./css/buttons.css"
 const NewGame = ( props ) => {
   return(
     <div>
-      <button  className="button-new-game" type="button" onClick={props.newGame} > New Game </button>
+      <button  className="button new-game" type="button" onClick={props.newGame} > New Game </button>
     </div>
   )
 }
